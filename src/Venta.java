6d653c5a42ba4aa250e251b2c1fe47f7ecd8022d
Milton Mariños <milcesar.mp98@gmail.com>
2020-05-25 16:38:35 -0500

    docs: Class Venta

--- src/Venta.java
+++ src/Venta.java
@@ -1,7 +1,8 @@
 
-
 /**
- *
+ * Clase que define los objetos de tipo Venta, la venta es realizada por
+ * empleado y registrado el sistema de la libreria
+ * 
  * @author Milton Mariños
  */
 public class Venta {
