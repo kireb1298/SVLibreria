1249d424b473c9fb8598a527c92e0a9b0fb103f0
Milton Mariños <milcesar.mp98@gmail.com>
2020-05-25 16:57:48 -0500

    feat: method generarCodigo

--- src/Venta.java
+++ src/Venta.java
@@ -1,4 +1,3 @@
-
 /**
  * Clase que define los objetos de tipo Venta, la venta es realizada por
  * empleado y registrado el sistema de la libreria
@@ -7,4 +6,18 @@
  */
 public class Venta {
     private String codigo;
+    private double subTotal;
+    private double impuesto;
+    private double total;
+    private static double igv;
+    private static int contador;
+
+    Empleado empleado;
+    Cliente cliente;
+    
+    private String generarCodigo() {
+        contador++;
+        codigo = "VE000000";
+        return codigo.substring(0, 8 - String.valueOf(contador).length()) + contador;
+    }
 }
