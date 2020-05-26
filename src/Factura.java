14023d66fd85d2b8822c4ee878b2f015adcd6cb3
jose lopez <jose2025lopez@gmail.com>
2020-05-26 14:43:10 -0500

    feat: atributos a la clase factura

--- src/Factura.java
+++ src/Factura.java
@@ -3,7 +3,12 @@
  * 
  * @see Comprobante
  * @author Milton Mariños
+ * atributos de la clase factura
+ * @author lopez jose
  */
 public class Factura extends Comprobante {
+    private String ruc;
+    private String razonSocial;
+    private int contador;
     
 }
