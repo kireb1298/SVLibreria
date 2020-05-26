1c6a600dfa3a4e178a5d051bbd78ed187b82e4e6
jose lopez <jose2025lopez@gmail.com>
2020-05-26 14:50:51 -0500

    feat: constructor de los atributos

--- src/Comprobante.java
+++ src/Comprobante.java
@@ -5,11 +5,26 @@ import java.util.Date;
  *
  * @author Milton Mariños
  * @author Piero Villarreal
+ * @author lopez jose
  */
 public class Comprobante {
     // Atributos
     private String codigo;
     private Date fecha;
     private String moneda;
+    /**
+     * Constructor de la clase comprobante con todos los atributos.
+     * 
+     * @param codigo
+     * @param fecha
+     * @param moneda
+     */
+    public Comprobante(String codigo, Date fecha, String moneda) {
+        this.codigo = codigo;
+        this.fecha = fecha;
+        this.moneda = moneda;
+    }
 
+    
+    
 }
