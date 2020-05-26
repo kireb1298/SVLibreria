ae1c87b5567ef4ca7ddcdfb83662ee99ac33403f
DrKoolz <33436715+DrKoolz@users.noreply.github.com>
2020-05-25 21:14:24 -0500

    feat : atributos comprobante de pago

--- src/Comprobante.java
+++ src/Comprobante.java
@@ -1,8 +1,21 @@
+import java.sql.Date;
 /**
  * Clase que define al comprobante de pago.
  *
  * @author Milton Mariños
+ * 
+ * Atributos del comprobante de pago.
+ * @autor Piero Villarreal
  */
 public class Comprobante {
+    private String codigo;
+    private Date fecha;
+    private String moneda;
 
+    public Comprobante(String codigo, Date fecha, String moneda) {
+        this.codigo = codigo;
+        this.fecha = fecha;
+        this.moneda = moneda;
+    }    
+    
 }
