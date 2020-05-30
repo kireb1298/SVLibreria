70db1be31df020953479dd64a7c6f3314e375f92
Milton Mariños <milcesar.mp98@gmail.com>
2020-05-30 04:15:38 -0500

    feat: metodo size RegistroEmpleado

--- src/RegistroEmpleado.java
+++ src/RegistroEmpleado.java
@@ -1,12 +1,12 @@
 import java.util.HashSet;
 
 /**
- * Clase que almacena y administra la coleccion de empleados
+ * Clase que almacena y administra la coleccion de empleados.
  * 
  * @author Milton Mariños
  */
 public class RegistroEmpleado {
-    HashSet<Empleado> empleados = new HashSet<Empleado>();
+    private HashSet<Empleado> empleados = new HashSet<Empleado>();
 
     /**
      * Metodo que registra un empleado del sistema.
@@ -27,7 +27,7 @@ public class RegistroEmpleado {
     }
 
     /**
-     * Metodo que realiza la busqueda de un empleado en base a su codigo
+     * Metodo que realiza la busqueda de un empleado en base a su codigo.
      * 
      * @param c dni del empleado que desea buscar
      * @return Si el empleado es encontrado se devuelve la direccion en memoria, en
@@ -38,5 +38,14 @@ public class RegistroEmpleado {
             if (e.getCodigo().equals(c))
                 return e;
         return null;
+    }
+
+    /**
+     * Metodo que retorna el numero de empleados registrados en el sistema.
+     * 
+     * @return Numero de empleados registrados en el sistema
+     */
+    public int size() {
+        return empleados.size();
     }
 }
